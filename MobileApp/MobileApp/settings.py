6d56f13f3914e876557a2b84fc3825adb3e037de
Dhruv Chopra <dhruv011@e.ntu.edu.sg>
2022-03-10 04:29:31 +0800

    Production changes Debug=False

--- MobileApp/MobileApp/settings.py
+++ MobileApp/MobileApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1v@+jvyhzmjh)y&xtkrx1rx^ijofa4e7j1tx5zy00*ei0#ah3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','dhruv.pythonanywhere.com']
 
 
 # Application definition
